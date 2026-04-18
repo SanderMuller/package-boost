2b6a4cf6e884a35765de207753ad744d115597f9
Sander Muller <github@scode.nl>
2026-04-18 15:31:53 +0200

    Fix CI: PHP 8.2 rejects class_alias of internal class
    Ship a plain stub file for Laravel\Boost\BoostServiceProvider and
    require it when the real class is absent, instead of aliasing stdClass.
    PHP 8.2 tightened class_alias() to reject internal classes as the
    first argument, breaking the test-time Boost stub on the matrix legs
    without laravel/boost installed.

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,7 +4,7 @@ use Orchestra\Testbench\TestCase;
 use SanderMuller\PackageBoost\PackageBoostServiceProvider;
 
 if (! class_exists('Laravel\\Boost\\BoostServiceProvider')) {
-    class_alias(stdClass::class, 'Laravel\\Boost\\BoostServiceProvider');
+    require __DIR__ . '/Stubs/BoostServiceProvider.php';
 }
 
 uses(TestCase::class)->in(__DIR__);
---
+++ tests/Stubs/BoostServiceProvider.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Laravel\Boost;
+
+if (! class_exists(BoostServiceProvider::class, false)) {
+    class BoostServiceProvider {}
+}
